e331d8119c98421c81d78a8590e810cbf9d85a5a
JUDHI3134 <judhistirbehera241@gmail.com>
2025-12-09 09:48:03 +0530

    create assets and category table

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,5 +1,5 @@
 import { relations } from "drizzle-orm";
-import { pgTable, text, timestamp, boolean, index } from "drizzle-orm/pg-core";
+import { pgTable, text, timestamp, boolean, index, serial, uuid, integer } from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
   id: text("id").primaryKey(),
@@ -96,3 +96,59 @@ export const accountRelations = relations(account, ({ one }) => ({
     references: [user.id],
   }),
 }));
+
+
+export const category = pgTable("category", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull().unique(),
+  createdAt: timestamp("created_at").$defaultFn(() => new Date()).notNull(),
+})
+
+export const asset = pgTable("asset", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  title: text("title").notNull(),
+  description: text("description"),
+  fileUrl: text("file_url").notNull(),
+  thumbnailUrl: text("thumbnail_url"),
+  isApproved: text("is_approved").default("pending").notNull(),
+  userId: text("user_id").notNull().references(() => user.id, { onDelete: 'cascade' }),
+  categoryId: integer("category_id").references(() => category.id),
+  createdAt: timestamp("created_at").$defaultFn(() => new Date()).notNull(),
+  updatedAt: timestamp("updated_at").$defaultFn(() => new Date()).notNull(),
+})
+
+export const usersRelations = relations(user, ({ many }) => ({
+  sessions: many(session),
+  accounts: many(account),
+  assets: many(asset),
+}))
+
+export const sessionsRelations = relations(session, ({ one }) => ({
+  user: one(user, {
+    fields: [session.userId],
+    references: [user.id],
+  })
+}))
+
+export const accountsRelations = relations(account, ({ one }) => ({
+  user: one(user, {
+    fields: [account.userId],
+    references: [user.id],
+  })
+}))
+
+export const categoryRelations = relations(category, ({ many }) => ({
+  asset: many(asset),
+}))
+
+export const assetsRelations = relations(asset, ({ one, many }) => ({
+  user: one(user, {
+    fields: [asset.userId],
+    references: [user.id]
+  }),
+  category: one(category, {
+    fields: [asset.categoryId],
+    references: [category.id]
+  })
+}))
+
